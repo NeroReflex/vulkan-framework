42944d00ebea7506358cf280ff50218e152615e6
Denis <benato.denis96@gmail.com>
2023-04-25 17:32:19 +0200

    using if let

--- vulkan_framework/src/queue.rs
+++ vulkan_framework/src/queue.rs
@@ -53,49 +53,43 @@ impl Queue {
                 let device = queue_family.get_parent_device();
                 let instance = device.get_parent_instance();
 
-                match instance.get_debug_ext_extension() {
-                    Some(ext) => {
-                        match debug_name {
-                            Some(name) => {
-                                for name_ch in name.as_bytes().iter() {
-                                    obj_name_bytes.push(*name_ch);
+                if let Some(ext) = instance.get_debug_ext_extension() {
+                    if let Some(name) = debug_name {
+                        for name_ch in name.as_bytes().iter() {
+                            obj_name_bytes.push(*name_ch);
+                        }
+                        obj_name_bytes.push(0x00);
+
+                        unsafe {
+                            let object_name = std::ffi::CStr::from_bytes_with_nul_unchecked(
+                                obj_name_bytes.as_slice(),
+                            );
+                            // set device name for debugging
+                            let dbg_info = ash::vk::DebugUtilsObjectNameInfoEXT::builder()
+                                .object_type(ash::vk::ObjectType::QUEUE)
+                                .object_handle(ash::vk::Handle::as_raw(queue))
+                                .object_name(object_name)
+                                .build();
+
+                            match ext.set_debug_utils_object_name(
+                                device.ash_handle().handle(),
+                                &dbg_info,
+                            ) {
+                                Ok(_) => {
+                                    #[cfg(debug_assertions)]
+                                    {
+                                        println!("Queue Debug object name changed");
+                                    }
                                 }
-                                obj_name_bytes.push(0x00);
-
-                                unsafe {
-                                    let object_name = std::ffi::CStr::from_bytes_with_nul_unchecked(
-                                        obj_name_bytes.as_slice(),
-                                    );
-                                    // set device name for debugging
-                                    let dbg_info = ash::vk::DebugUtilsObjectNameInfoEXT::builder()
-                                        .object_type(ash::vk::ObjectType::QUEUE)
-                                        .object_handle(ash::vk::Handle::as_raw(queue))
-                                        .object_name(object_name)
-                                        .build();
-
-                                    match ext.set_debug_utils_object_name(
-                                        device.ash_handle().handle(),
-                                        &dbg_info,
-                                    ) {
-                                        Ok(_) => {
-                                            #[cfg(debug_assertions)]
-                                            {
-                                                println!("Queue Debug object name changed");
-                                            }
-                                        }
-                                        Err(err) => {
-                                            #[cfg(debug_assertions)]
-                                            {
-                                                panic!("Error setting the Debug name for the newly created Queue, will use handle. Error: {}", err);
-                                            }
-                                        }
+                                Err(err) => {
+                                    #[cfg(debug_assertions)]
+                                    {
+                                        panic!("Error setting the Debug name for the newly created Queue, will use handle. Error: {}", err);
                                     }
                                 }
                             }
-                            None => {}
-                        };
+                        }
                     }
-                    None => {}
                 }
 
                 Ok(Arc::new(Self {
