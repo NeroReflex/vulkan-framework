7ae522827575e0f673f9dcc4d152bb34f253acbb
Denis Benato <benato.denis96@gmail.com>
2025-08-09 20:56:04 +0200

    silence the compiler

--- engine/src/rendering/system.rs
+++ engine/src/rendering/system.rs
@@ -157,7 +157,7 @@ impl System {
             .load_object(PathBuf::from("crytek_sponza.tar"))
             .unwrap();
 
-        manager.add_instance(sponza_object_id, IDENTITY_MATRIX);
+        manager.add_instance(sponza_object_id, IDENTITY_MATRIX).unwrap();
     }
 
     pub fn new(
